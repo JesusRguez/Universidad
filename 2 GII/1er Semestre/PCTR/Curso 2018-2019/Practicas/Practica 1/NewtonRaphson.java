4a50c13beb9d47394d365ae7f8753d1fa1afb490
JesusRguez <jesusrh1997@gmail.com>
2018-10-02 10:06:57 +0200

    Ejercicio 1.2

---
+++ 2 GII/1er Semestre/PCTR/Curso 2018-2019/Practicas/Practica 1/NewtonRaphson.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.Math;
+
+class NewtonRaphson{
+
+    public static void coseno(double aprox, int iteraciones){
+        for (int i=0; i<iteraciones; ++i) {
+            aprox=aprox-((Math.cos(aprox)-Math.pow(aprox,3))/(-Math.sin(aprox)-3*Math.pow(aprox,2)));
+            System.out.println((i+1)+". "+aprox);
+        }
+    }
+
+    public static void cuadrado(double aprox, int iteraciones){
+        for (int i=0; i<iteraciones; ++i) {
+            aprox=aprox-((Math.pow(aprox,2)-5)/(2*aprox));
+            System.out.println((i+1)+". "+aprox);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Seleccione la función que desea aproximar:\n1. f(x)=cos(x)-x³ en [0, 1]\n2. f(x)=x²-5 en [2, 3]");
+        Scanner teclado = new Scanner(System.in);
+        int opcion = teclado.nextInt();
+        System.out.println("Introduzca la aproximación inicial:");
+        double aprox = teclado.nextDouble();
+        System.out.println("Introduzca el número de iteraciones:");
+        int iteraciones = teclado.nextInt();
+        switch (opcion) {
+            case 1: coseno(aprox, iteraciones); break;
+            case 2: cuadrado(aprox, iteraciones); break;
+        }
+    }
+}
