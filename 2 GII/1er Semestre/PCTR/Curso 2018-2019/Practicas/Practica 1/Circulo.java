a93d6c1ec967cce847262550381c26788aba250c
JesusRguez <jesusrh1997@gmail.com>
2018-10-02 09:37:09 +0200

    Hecho el ejercicio 1 de la práctica 1

---
+++ 2 GII/1er Semestre/PCTR/Curso 2018-2019/Practicas/Practica 1/Circulo.java
@@ -0,0 +1,10 @@
+class Circulo{
+    static final double PI = 3.1416;
+    public static void main(String[] args) {
+        double diametro = 14.2;
+        double altura = 20;
+        double radio = diametro/2;
+        double volumen = (PI*radio*radio*altura)/3;
+        System.out.println("El volumen del cono es: "+volumen);
+    }
+}
