0dbb3d14c235ad31fe081196b1ff0201c142a6ed
Jesús Rodríguez Heras <jesusrh1997@gmail.com>
2018-10-16 15:17:55 +0200

    Añadido el paquete elipse

--- 2 GII/1er Semestre/PCTR/Curso 2018-2019/Practicas/Practica 2/complejos/Complejos.java
+++ 2 GII/1er Semestre/PCTR/Curso 2018-2019/Practicas/Practica 2/complejos/Complejos.java
@@ -10,6 +10,12 @@ class Complejos{
 
     Complejos(){}
 
+    /**
+     * Método para sumar complejos
+     * @param  a Primer complejo
+     * @param  b Segundo complejo
+     * @return   Retorna el complejo resultante
+     */
     public static Complejos suma(Complejos a, Complejos b){
         Complejos res = new Complejos();
         res.real = a.real + b.real;
@@ -17,6 +23,12 @@ class Complejos{
         return res;
     }
 
+    /**
+     * Método para restar complejos
+     * @param  a Primero complejo
+     * @param  b Segundo complejo
+     * @return   Retorna el complejo resultante
+     */
     public static Complejos resta(Complejos a, Complejos b){
         Complejos res = new Complejos();
         res.real = a.real - b.real;
@@ -24,11 +36,22 @@ class Complejos{
         return res;
     }
 
+    /**
+     * Método para calcular el módulo de un complejo
+     * @param  a Complejo al que calcular el módulo
+     * @return   Retorna el módulo del complejo
+     */
     public static double modulo(Complejos a){
         double res = Math.sqrt(Math.pow(a.real, 2)+(Math.pow(a.imaginario, 2)));
         return res;
     }
 
+    /**
+     * Método para calcular el producto de complejos
+     * @param  a Primer complejo
+     * @param  b Segundo complejo
+     * @return   Retorna el producto de dos complejos
+     */
     public static Complejos producto(Complejos a, Complejos b){
         Complejos res = new Complejos();
         res.real = a.real * b.imaginario;
@@ -36,6 +59,12 @@ class Complejos{
         return res;
     }
 
+    /**
+     * Método para calcular el cociente de complejos
+     * @param  a Primer complejo
+     * @param  b Segundo complejo
+     * @return   Retorna el cociente de dos complejos
+     */
     public static Complejos cociente(Complejos a, Complejos b){
         Complejos res = new Complejos();
         res.real = (a.real * b.real + a.imaginario * b.imaginario) / (Math.pow(b.real, 2) + Math.pow(b.imaginario, 2));
---
+++ 2 GII/1er Semestre/PCTR/Curso 2018-2019/Practicas/Practica 2/elipse/usaElipse.java
@@ -0,0 +1,9 @@
+/**
+ * @author Jesús Rodríguez Heras
+ */
+
+package elipse;
+
+class usaElipse{
+
+}
