99b6e53353d099320aa44067af8441f999317379
Jesús Rodríguez Heras <jesusrh1997@gmail.com>
2018-10-16 14:17:25 +0200

    Practica 2.3

--- 2 GII/1er Semestre/PCTR/Curso 2018-2019/Practicas/Practica 2/Estadistica.java
+++ 2 GII/1er Semestre/PCTR/Curso 2018-2019/Practicas/Practica 2/Estadistica.java
@@ -14,7 +14,7 @@ class Estadistica{
         return s/v.length;
     }
 
-    public static void moda(double []v){
+    public static double moda(double []v){
         int repeticiones;
         int maxrepeticiones = 0;
         double moda = 0;
@@ -30,20 +30,20 @@ class Estadistica{
                 }
             }
         }
-        System.out.println("La moda es: "+moda);
+        return moda;
     }
 
-    public static void varianza(double []v){
+    public static double varianza(double []v){
         double sumatorio = 0;
         double m = media(v);
         for (int i=0; i<v.length; ++i) {
             sumatorio+=Math.pow(v[i]-m, 2);
         }
-        System.out.println("La varianza es "+sumatorio/(v.length-1));
+        return sumatorio/(v.length-1);
     }
 
-    public static void desviacion(double []v){
-
+    public static double desviacion(double []v){
+        return Math.sqrt(varianza(v));
     }
 
     public static void main(String[] args) {
@@ -57,24 +57,32 @@ class Estadistica{
         }
         int opcion;
         do {
-            System.out.println("Seleccione la acción que desea:\n1. Media\n2. Moda\n3. Varianza\n4. Desviación típica");
-            opcion=teclado.nextInt();
-            switch(opcion){
-                case 1:
-                    double m = media(v);
-                    System.out.println("La media es "+m);
-                    break;
-                case 2: moda(v);
-                    break;
-                case 3: varianza(v);
-                    break;
-                case 4: desviacion(v);
-                    break;
-                case 5: break;
-            }
-            if (opcion > 5) {
-                System.out.println("Solo hay cuatro opciones, por favor, selecciones 1, 2, 3, 4 ó 5.");
-            }
-        } while (opcion < 1 && opcion > 5);
+            do {
+                System.out.println("Seleccione la acción que desea:\n1. Media\n2. Moda\n3. Varianza\n4. Desviación típica\n5. Salir");
+                opcion=teclado.nextInt();
+                switch(opcion){
+                    case 1:
+                        double me = media(v);
+                        System.out.println("La media es "+me);
+                        break;
+                    case 2:
+                        double mo = moda(v);
+                        System.out.println("La moda es "+mo);
+                        break;
+                    case 3:
+                        double va = varianza(v);
+                        System.out.println("La varianza es "+va);
+                        break;
+                    case 4:
+                        double d = desviacion(v);
+                        System.out.println("La desviación es "+d);
+                        break;
+                    case 5: break;
+                }
+                if (opcion > 5) {
+                    System.out.println("Solo hay cuatro opciones, por favor, selecciones 1, 2, 3, 4 ó 5.");
+                }
+            } while (opcion < 1 && opcion > 5);
+        } while (opcion != 5);        
     }
 }
