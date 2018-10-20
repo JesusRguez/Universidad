5570168495b51ab502fddb06f711ae7758e0fe81
Jesús Rodríguez Heras <jesusrh1997@gmail.com>
2018-10-20 12:47:56 +0200

    Práctica 3

---
+++ 2 GII/1er Semestre/PCTR/Curso 2018-2019/Prácticas/Práctica 3/poligono/Poligono.java
@@ -0,0 +1,59 @@
+/**
+ * @author Jesús Rodríguez Heras
+ * @version 1.0
+ */
+
+package poligono;
+
+import java.util.*;
+
+class Poligono{
+    public static ArrayList<Punto> figura;
+    public static Scanner teclado = new Scanner(System.in);
+
+    /**
+     * Constructor de la clase Poligono
+     * @param nueva figura nueva que le llega como parámetro de entrada
+     */
+    public Poligono(ArrayList<Punto> nueva){
+        figura = new ArrayList<Punto>();
+        figura = nueva;
+    }
+
+    /**
+     * Método para calcular el número de lados de una figura
+     * @return Devuelve el número de lados que coincide con el número de puntos de la figura
+     */
+    public int nLados(){
+        return figura.size();
+    }
+
+    /**
+     * Método para desplazar una figura existente
+     */
+    public void desplazarFigura(){
+        System.out.println("Introduzca el incremento en el eje X: ");
+        double desplazamientoX = teclado.nextDouble();
+        System.out.println("Introduzca el incremento en el eje Y: ");
+        double desplazamientoY = teclado.nextDouble();
+        for (int i=0; i<figura.size(); ++i) {
+            figura.get(i).moverEn(desplazamientoX, desplazamientoY);
+        }
+    }
+
+    /**
+     * Método para hacer zoom a una figura existente
+     */
+    public void zoom(){
+        System.out.println("Introduzca la escala para hacer zoom a la imagen: ");
+        double escala = teclado.nextDouble();
+        for (int i=0; i<figura.size(); ++i) {
+            figura.get(i).x = figura.get(i).x * escala;
+            figura.get(i).y = figura.get(i).y * escala;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 2 GII/1er Semestre/PCTR/Curso 2018-2019/Prácticas/Práctica 3/poligono/Punto.java
@@ -0,0 +1,46 @@
+/**
+ * @author Jesús Rodríguez Heras
+ * @version 1.0
+ */
+
+package poligono;
+
+public class Punto {
+    double x, y; // Variables de instancia
+
+    /**
+     * Constructor de la clase Punto
+     * @param x Coordenada X del punto
+     * @param y Coordenada Y del punto
+     */
+    public Punto(double x, double y) {
+        this.x= x; this.y= y;
+    }
+
+    /**
+     * Método para mover un punto
+     * @param dx Incremento de la coordenada X para mover un punto en el eje de abscisas
+     * @param dy Incremento de la coordenada Y para mover un punto en el eje de ordenadas
+     */
+    public void moverEn(double dx, double dy) {
+        this.x+= dx; this.y+= dy;
+    }
+
+    /**
+     * Método para calcular la distancia entre dos puntos
+     * @param  a Primer punto
+     * @param  b Segundo punto
+     * @return   Retorna la distancia entre los puntos a y b
+     */
+    public double distancia(Punto a, Punto b){
+        return Math.abs(Math.sqrt(Math.pow((b.x - a.x), 2) + Math.pow((b.y - a.y), 2)));
+    }
+
+    /**
+     * Método toString de la clase Punto
+     * @return Retorna un punto como un String
+     */
+    public String toString() {
+        return "("+this.x+","+this.y+")";
+    }
+}
