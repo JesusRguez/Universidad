a4479d7292bc38df93d48f5c41cb5007d323fd7b
JesusRguez <jesusrh1997@gmail.com>
2018-11-02 17:39:05 +0100

    Clase DA

--- 3 GII/1er Semestre/DA/Prácticas/Práctica 1/RODRIGUEZ_HERAS_JESUS/p1/DefenseStrategy.cpp
+++ 3 GII/1er Semestre/DA/Prácticas/Práctica 1/RODRIGUEZ_HERAS_JESUS/p1/DefenseStrategy.cpp
@@ -40,27 +40,34 @@ Vector3 cellCenterToPosition(int i, int j, float cellWidth, float cellHeight){ r
 // cellHeight - alto de las celdas
 void positionToCell(const Vector3 pos, int &i_out, int &j_out, float cellWidth, float cellHeight){ i_out = (int)(pos.y * 1.0f/cellHeight); j_out = (int)(pos.x * 1.0f/cellWidth); }
 
-bool factibilidad(int fila, int coloumna, Defense* defensa, std::list<Object*> obstaculos, float mapWidth, float mapHeight, std::list<Defense*> defensas, int nCellsWidth, int nCellsHeight){
+bool factibilidad(int fila, int columna, Defense* defensa, std::list<Object*> obstaculos, float mapWidth, float mapHeight, std::list<Defense*> defensas, int nCellsWidth, int nCellsHeight){
 	bool entra = true;
 	Vector3 posicionDefensa = cellCenterToPosition(fila, columna, cellWidth, cellHeight));
-	if (posicionDefensa.x-d.radio < 0 || posicionDefensa.x+d.radio > mapWidth || posicionDefensa.y-d.radio < 0 || posicionDefensa.y+d.radio > 0) {
+	if (posicionDefensa.x-defensa.radio < 0 || posicionDefensa.x+defensa.radio > mapWidth || posicionDefensa.y-defensa.radio < 0 || posicionDefensa.y+defensa.radio > 0) {
 		entra = false;//no cabe porque se sale de los límites del mapa
 	}else{
 		std::list<Object*>::const_iterator i = obstaculos.begin();
 		std::list<Defense*>::const_iterator j = defensas.begin();
 		while (i!=obstaculos.end()) {
-			if ((d.radio + obstaculos[i].radio) > (_distance(posicionDefensa, obstaculos[i].position))) {
+			if ((defensa.radio + obstaculos[i].radio) > (_distance(posicionDefensa, obstaculos[i].position))) {
 				entra = false;
 			}else{
-				while (j!=i) {
-					if ((d.radio + defensas[j].radio) > (_distance(posicionDefensa, defensas[j].position))) {
+				while (j!=defensas.end()) {
+					if ((defensa.radio + defensas[j].radio) > (_distance(posicionDefensa, defensas[j].position))) {
 						entra = false;
 					}
+					++j;
 				}
 			}
+			++i;
 		}
 	}
 	return entra;
+}
+
+Defense& seleccion(std::list<Defense*> defensas, std::list<Obstacles*> obstaculos, float mapWidth, float mapHeight){
+	//Ahora la cuestión es seleccionar la celda que mejor se adapta a nuestras especificaciones
+
 }
 
 void DEF_LIB_EXPORTED placeDefenses(bool** freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight, std::list<Object*> obstacles, std::list<Defense*> defenses) {
@@ -69,14 +76,31 @@ void DEF_LIB_EXPORTED placeDefenses(bool** freeCells, int nCellsWidth, int nCell
     float cellHeight = mapHeight / nCellsHeight;
 
     int maxAttemps = 1000;
-    List<Defense*>::iterator currentDefense = defenses.begin();
+    /*List<Defense*>::iterator currentDefense = defenses.begin();
     while(currentDefense != defenses.end() && maxAttemps > 0) {
 
         (*currentDefense)->position.x = ((int)(_RAND2(nCellsWidth))) * cellWidth + cellWidth * 0.5f;
         (*currentDefense)->position.y = ((int)(_RAND2(nCellsHeight))) * cellHeight + cellHeight * 0.5f;
         (*currentDefense)->position.z = 0;
         ++currentDefense;
-    }
+    }*/
+
+	Defense d;
+	std::list<Defenses> defensesCopia = defenses;
+	std::list<Defense*>::iterator currentDefense = defensesCopia.begin();
+	while (currentDefense != defensesCopia.end() && masAttemps > 0) {
+		d = seleccion(defensesCopia, obstacles, mapWidth, mapHeight);
+		//defenses.remove(d); No puede ser así porque solo tnemos una lista de defensas que no podemos perder
+		defensesCopia.remove(d);
+		int fila = 0, columna = 0;
+		positionToCell(d.position, fila, columna, cellWidth, cellHeight);
+		if (factibilidad(fila, columna, d, obstacles, mapWidth, mapHeight, defenses, nCellsWidth, nCellsHeight)) {
+			//defenses.insert(d); Por la misma razón que antes, debe de ser la misma, sin insertar nada
+			//Es posible que aquí haya que modificar la matriz de freeCells
+			freeCells[fila][columna] = false; //Luego, gracias a la función de factibiliad probaremos si entra la siguiente defensa o no
+		}
+		++currentDefense;
+	}
 
 #ifdef PRINT_DEFENSE_STRATEGY
 
