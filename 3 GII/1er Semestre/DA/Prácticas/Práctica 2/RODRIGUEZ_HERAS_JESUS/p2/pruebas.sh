3b2ae0dd967eeb77b24b94124bd690f0447429d7
JesusRguez <jesusrh1997@gmail.com>
2018-11-27 09:25:07 +0100

    Cambios mínimos

--- 3 GII/1er Semestre/DA/Prácticas/Práctica 2/RODRIGUEZ_HERAS_JESUS/p2/pruebas.sh
+++ 3 GII/1er Semestre/DA/Prácticas/Práctica 2/RODRIGUEZ_HERAS_JESUS/p2/pruebas.sh
@@ -4,7 +4,7 @@ pases=(0.5 0.6 0.4 0.5 0.7 1 0.5 0.6 0.3 0.5)
 defenses=(30 40 40 50 40 50 40 60 70 80)
 defensetypes=(30 40 30 40 20 50 40 50 70 70)
 defenserandomness=(0.5 0.6 0.4 0.5 0.7 0.5 0.5 0.6 0.3 0.5)
-for j in {0..8}
+for j in {0..7}
 do
     echo "Nivel ${niveles[j]}: "
     ../simulador/simulador -level ${niveles[j]} -pases ${pases[j]} -defenses ${defenses[j]} -defensetypes ${defensetypes[j]} -defenserandomness ${defenserandomness[j]} -time 1000
