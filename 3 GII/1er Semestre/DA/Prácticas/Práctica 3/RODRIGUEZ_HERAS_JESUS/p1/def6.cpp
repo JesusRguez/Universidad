e37a6adfb6f44eb0c751228a41ac9978042ab5d0
JesusRguez <jesusrh1997@gmail.com>
2018-12-04 19:13:43 +0100

    Práctica DA

---
+++ 3 GII/1er Semestre/DA/Prácticas/Práctica 3/RODRIGUEZ_HERAS_JESUS/p1/def6.cpp
@@ -0,0 +1,29 @@
+void DEF_LIB_EXPORTED placeDefenses(bool** freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight, std::list<Object*> obstacles, std::list<Defense*> defenses) {
+
+    float cellWidth = mapWidth / nCellsWidth;
+    float cellHeight = mapHeight / nCellsHeight;
+    int maxAttemps = 1000;
+    int fila = 0, columna = 0;
+    float x = 0, y = 0;
+
+    // ...
+	// Ejercicio 3
+    // ...
+    // Continuacion del ejercicio 3
+
+    while (currentDefense != defenses.end() && maxAttemps > 0) {
+        seleccion(mapa, nCellsWidth, nCellsHeight, &fila, &columna);
+        x = fila*cellWidth + cellWidth*0.5f;
+        y = columna*cellHeight + cellHeight*0.5f;
+        if(factibilidad(x, y, (*currentDefense), obstacles, mapWidth, mapHeight, defenses, cellWidth, cellHeight)){
+            (*currentDefense)->position.x = x;
+            (*currentDefense)->position.y = y;
+            (*currentDefense)->position.z = 0;
+            ++currentDefense;
+        }
+        --maxAttemps;
+    }
+
+    // Resto de codigo de PRINT_DEFENSE_STRATEGY que no hemos modificado
+
+}
--- 3 GII/1er Semestre/DA/Prácticas/Práctica 3/RODRIGUEZ_HERAS_JESUS/p3/DefenseStrategy.cpp
+++ 3 GII/1er Semestre/DA/Prácticas/Práctica 3/RODRIGUEZ_HERAS_JESUS/p3/DefenseStrategy.cpp
@@ -12,8 +12,7 @@
 
 using namespace Asedio;
 
-float defaultCellValue(int row, int col, bool** freeCells, int nCellsWidth, int nCellsHeight
-    , float mapWidth, float mapHeight, List<Object*> obstacles, List<Defense*> defenses) {
+float defaultCellValue(int row, int col, bool** freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight, List<Object*> obstacles, List<Defense*> defenses) {
 
     float cellWidth = mapWidth / nCellsWidth;
     float cellHeight = mapHeight / nCellsHeight;
@@ -28,26 +27,317 @@ float defaultCellValue(int row, int col, bool** freeCells, int nCellsWidth, int 
     return val;
 }
 
+bool factibilidad(float x, float y, Defense* defensa, std::list<Object*> obstaculos, float mapWidth, float mapHeight, std::list<Defense*> defensas, float cellWidth, float cellHeight){
+
+	bool entra = true;
+	if (x-defensa->radio < 0 || x+defensa->radio > mapWidth || y-defensa->radio < 0 || y+defensa->radio > mapHeight) {
+		entra = false; //No cabe porque se sale de los límites del mapa
+	}
+
+	std::list<Object*>::const_iterator iterObst = obstaculos.begin();
+	std::list<Defense*>::const_iterator iterDef = defensas.begin();
+	Vector3 posicionDefensa(x, y, 0);
+	while (iterObst!=obstaculos.end()) {
+		if ((defensa->radio + (*iterObst)->radio) > (_distance(posicionDefensa, (*iterObst)->position))) {
+			entra = false; //Se choca con un obsaculo
+		}else{
+			while ((*iterDef)!=defensa) {
+				if ((defensa->radio + (*iterDef)->radio) > (_distance(posicionDefensa, (*iterDef)->position))) {
+					entra = false; //Se choca con una defensa
+				}
+				++iterDef;
+			}
+		}
+		++iterObst;
+	}
+	return entra;
+}
+
+void seleccionSinOrdenacion(float** mapa, int nCellsWidth, int nCellsHeight, int* fila, int* columna){
+	float maxi = 0;
+	for (size_t i = 0; i < nCellsWidth; ++i) {
+		for (size_t j = 0; j < nCellsHeight; ++j) {
+			if (mapa[i][j] > maxi) {
+				maxi = mapa[i][j];
+				*fila = i;
+				*columna = j;
+			}
+		}
+	}
+	mapa[*fila][*columna] = 0;
+}
+
+void DEF_LIB_EXPORTED placeDefensesSinOrdenacion(bool** freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight, std::list<Object*> obstacles, std::list<Defense*> defenses) {
+
+    float cellWidth = mapWidth / nCellsWidth;
+    float cellHeight = mapHeight / nCellsHeight;
+    int maxAttemps = 1000;
+
+	float** mapa = new float*[nCellsHeight];
+	for (size_t i = 0; i < nCellsHeight; ++i) {
+		mapa[i] = new float[nCellsWidth];
+	}
+
+	for (size_t i = 0; i < nCellsHeight; ++i) {
+		for (size_t j = 0; j < nCellsHeight; ++j) {
+			mapa[i][j] = defaultCellValue(i, j, freeCells, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, defenses);
+		}
+	}
+
+	int fila = 0, columna = 0;
+	float x = 0, y = 0;
+
+	std::list<Defense*>::iterator currentDefense = defenses.begin();
+	while(currentDefense == defenses.begin() && maxAttemps > 0){
+		seleccionSinOrdenacion(mapa, nCellsWidth, nCellsHeight, &fila, &columna);
+		x = fila*cellWidth + cellWidth*0.5f;
+		y = columna*cellHeight + cellHeight*0.5f;
+		if(factibilidad(x, y, (*currentDefense), obstacles, mapWidth, mapHeight, defenses, cellWidth, cellHeight)){
+			(*currentDefense)->position.x = x;
+			(*currentDefense)->position.y = y;
+			(*currentDefense)->position.z = 0;
+
+			//defaultCellValue(mapa, nCellsWidth, nCellsHeight, cellWidth, cellHeight, mapWidth, mapHeight, defenses);
+
+			++currentDefense;
+		}
+		--maxAttemps;
+	}
+
+	while (currentDefense != defenses.end() && maxAttemps > 0) {
+		seleccionSinOrdenacion(mapa, nCellsWidth, nCellsHeight, &fila, &columna);
+		x = fila*cellWidth + cellWidth*0.5f;
+		y = columna*cellHeight + cellHeight*0.5f;
+		if(factibilidad(x, y, (*currentDefense), obstacles, mapWidth, mapHeight, defenses, cellWidth, cellHeight)){
+			(*currentDefense)->position.x = x;
+			(*currentDefense)->position.y = y;
+			(*currentDefense)->position.z = 0;
+			++currentDefense;
+		}
+		--maxAttemps;
+	}
+}
+
+void DEF_LIB_EXPORTED placeDefensesFusion(bool** freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight, std::list<Object*> obstacles, std::list<Defense*> defenses) {
+
+    float cellWidth = mapWidth / nCellsWidth;
+    float cellHeight = mapHeight / nCellsHeight;
+    int maxAttemps = 1000;
+
+	float** mapa = new float*[nCellsHeight];
+	for (size_t i = 0; i < nCellsHeight; ++i) {
+		mapa[i] = new float[nCellsWidth];
+	}
+
+	for (size_t i = 0; i < nCellsHeight; ++i) {
+		for (size_t j = 0; j < nCellsHeight; ++j) {
+			mapa[i][j] = defaultCellValue(i, j, freeCells, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, defenses);
+		}
+	}
+
+    std::vector<float> mapaOrdenado(0, nCellsHeight*nCellsWidth);
+    std::vector<float>::iterator itMapa = mapaOrdenado.begin();
+    for (size_t i = 0; i < nCellsHeight; ++i) {
+        for (size_t j = 0; j < nCellsWidth; ++j) {
+            mapaOrdenado.insert(itMapa, mapa[i][j]);
+            ++itMapa;
+        }
+    }
+
+	int fila = 0, columna = 0;
+	float x = 0, y = 0;
+
+	std::list<Defense*>::iterator currentDefense = defenses.begin();
+	while(currentDefense == defenses.begin() && maxAttemps > 0){
+		seleccionSinOrdenacion(mapa, nCellsWidth, nCellsHeight, &fila, &columna);
+		x = fila*cellWidth + cellWidth*0.5f;
+		y = columna*cellHeight + cellHeight*0.5f;
+		if(factibilidad(x, y, (*currentDefense), obstacles, mapWidth, mapHeight, defenses, cellWidth, cellHeight)){
+			(*currentDefense)->position.x = x;
+			(*currentDefense)->position.y = y;
+			(*currentDefense)->position.z = 0;
+
+			//defaultCellValue(mapa, nCellsWidth, nCellsHeight, cellWidth, cellHeight, mapWidth, mapHeight, defenses);
+
+			++currentDefense;
+		}
+		--maxAttemps;
+	}
+
+	while (currentDefense != defenses.end() && maxAttemps > 0) {
+		seleccionSinOrdenacion(mapa, nCellsWidth, nCellsHeight, &fila, &columna);
+		x = fila*cellWidth + cellWidth*0.5f;
+		y = columna*cellHeight + cellHeight*0.5f;
+		if(factibilidad(x, y, (*currentDefense), obstacles, mapWidth, mapHeight, defenses, cellWidth, cellHeight)){
+			(*currentDefense)->position.x = x;
+			(*currentDefense)->position.y = y;
+			(*currentDefense)->position.z = 0;
+			++currentDefense;
+		}
+		--maxAttemps;
+	}
+}
+
+
+void DEF_LIB_EXPORTED placeDefensesRapido(bool** freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight, std::list<Object*> obstacles, std::list<Defense*> defenses) {
+
+    float cellWidth = mapWidth / nCellsWidth;
+    float cellHeight = mapHeight / nCellsHeight;
+    int maxAttemps = 1000;
+
+	float** mapa = new float*[nCellsHeight];
+	for (size_t i = 0; i < nCellsHeight; ++i) {
+		mapa[i] = new float[nCellsWidth];
+	}
+
+	for (size_t i = 0; i < nCellsHeight; ++i) {
+		for (size_t j = 0; j < nCellsHeight; ++j) {
+			mapa[i][j] = defaultCellValue(i, j, freeCells, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, defenses);
+		}
+	}
+
+	int fila = 0, columna = 0;
+	float x = 0, y = 0;
+
+	std::list<Defense*>::iterator currentDefense = defenses.begin();
+	while(currentDefense == defenses.begin() && maxAttemps > 0){
+		seleccionSinOrdenacion(mapa, nCellsWidth, nCellsHeight, &fila, &columna);
+		x = fila*cellWidth + cellWidth*0.5f;
+		y = columna*cellHeight + cellHeight*0.5f;
+		if(factibilidad(x, y, (*currentDefense), obstacles, mapWidth, mapHeight, defenses, cellWidth, cellHeight)){
+			(*currentDefense)->position.x = x;
+			(*currentDefense)->position.y = y;
+			(*currentDefense)->position.z = 0;
+
+			//defaultCellValue(mapa, nCellsWidth, nCellsHeight, cellWidth, cellHeight, mapWidth, mapHeight, defenses);
+
+			++currentDefense;
+		}
+		--maxAttemps;
+	}
+
+	while (currentDefense != defenses.end() && maxAttemps > 0) {
+		seleccionSinOrdenacion(mapa, nCellsWidth, nCellsHeight, &fila, &columna);
+		x = fila*cellWidth + cellWidth*0.5f;
+		y = columna*cellHeight + cellHeight*0.5f;
+		if(factibilidad(x, y, (*currentDefense), obstacles, mapWidth, mapHeight, defenses, cellWidth, cellHeight)){
+			(*currentDefense)->position.x = x;
+			(*currentDefense)->position.y = y;
+			(*currentDefense)->position.z = 0;
+			++currentDefense;
+		}
+		--maxAttemps;
+	}
+}
+
+void DEF_LIB_EXPORTED placeDefensesMonticulo(bool** freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight, std::list<Object*> obstacles, std::list<Defense*> defenses) {
+
+    float cellWidth = mapWidth / nCellsWidth;
+    float cellHeight = mapHeight / nCellsHeight;
+    int maxAttemps = 1000;
+
+	float** mapa = new float*[nCellsHeight];
+	for (size_t i = 0; i < nCellsHeight; ++i) {
+		mapa[i] = new float[nCellsWidth];
+	}
+
+	for (size_t i = 0; i < nCellsHeight; ++i) {
+		for (size_t j = 0; j < nCellsHeight; ++j) {
+			mapa[i][j] = defaultCellValue(i, j, freeCells, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, defenses);
+		}
+	}
+
+    std::vector<float> mapaOrdenado(0, nCellsHeight*nCellsWidth);
+    std::vector<float>::const_iterator itMapa = mapaOrdenado.begin();
+    for (size_t i = 0; i < nCellsHeight; ++i) {
+        for (size_t j = 0; j < nCellsWidth; ++j) {
+            mapaOrdenado.insert(itMapa, mapa[i][j]);
+            ++itMapa;
+        }
+    }
+
+    std::make_heap(mapaOrdenado.begin(), mapaOrdenado.end());
+    std::sort_heap(mapaOrdenado.begin(), mapaOrdenado.end(), std::greater<float>());
+
+	int fila = 0, columna = 0;
+	float x = 0, y = 0;
+
+	std::list<Defense*>::iterator currentDefense = defenses.begin();
+	while(currentDefense == defenses.begin() && maxAttemps > 0){
+		seleccionSinOrdenacion(mapa, nCellsWidth, nCellsHeight, &fila, &columna);
+		x = fila*cellWidth + cellWidth*0.5f;
+		y = columna*cellHeight + cellHeight*0.5f;
+		if(factibilidad(x, y, (*currentDefense), obstacles, mapWidth, mapHeight, defenses, cellWidth, cellHeight)){
+			(*currentDefense)->position.x = x;
+			(*currentDefense)->position.y = y;
+			(*currentDefense)->position.z = 0;
+
+			//defaultCellValue(mapa, nCellsWidth, nCellsHeight, cellWidth, cellHeight, mapWidth, mapHeight, defenses);
+
+			++currentDefense;
+		}
+		--maxAttemps;
+	}
+
+	while (currentDefense != defenses.end() && maxAttemps > 0) {
+		seleccionSinOrdenacion(mapa, nCellsWidth, nCellsHeight, &fila, &columna);
+		x = fila*cellWidth + cellWidth*0.5f;
+		y = columna*cellHeight + cellHeight*0.5f;
+		if(factibilidad(x, y, (*currentDefense), obstacles, mapWidth, mapHeight, defenses, cellWidth, cellHeight)){
+			(*currentDefense)->position.x = x;
+			(*currentDefense)->position.y = y;
+			(*currentDefense)->position.z = 0;
+			++currentDefense;
+		}
+		--maxAttemps;
+	}
+}
+
 void DEF_LIB_EXPORTED placeDefenses3(bool** freeCells, int nCellsWidth, int nCellsHeight, float mapWidth, float mapHeight, List<Object*> obstacles, List<Defense*> defenses) {
 
     float cellWidth = mapWidth / nCellsWidth;
     float cellHeight = mapHeight / nCellsHeight;
 
-	cronometro c; // hacer 4 cronometros porque habrá que repetir cuatro veces: una sin ordenacion, otra con ordenacion por fusion, otra por ordenación rapida y otra con montículo
-    long int r = 0;
-    c.activar();
+	cronometro c1;
+    long int r1 = 0;
+    c1.activar();
     do { // Este do-while tiene el esequema de medida adaptativo
-		List<Defense*>::iterator currentDefense = defenses.begin();
-		while(currentDefense != defenses.end() && maxAttemps > 0) {
+		placeDefensesSinOrdenacion(freeCells, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, defenses);
 
-			(*currentDefense)->position.x = ((int)(_RAND2(nCellsWidth))) * cellWidth + cellWidth * 0.5f;
-			(*currentDefense)->position.y = ((int)(_RAND2(nCellsHeight))) * cellHeight + cellHeight * 0.5f;
-			(*currentDefense)->position.z = 0;
-			++currentDefense; // quitamos estas 4 líneas como en la p1
-		}
+		++r1;
+    } while(c1.tiempo() < 1.0);
+    c1.parar();
 
-		++r;
-    } while(c.tiempo() < 1.0);
-    c.parar();
-    std::cout << (nCellsWidth * nCellsHeight) << '\t' << c.tiempo() / r << '\t' << c.tiempo() / r << '\t' << c.tiempo() / r << '\t' << c.tiempo() / r << std::endl;
+    cronometro c2;
+    long int r2 = 0;
+    c2.activar();
+    do { // Este do-while tiene el esequema de medida adaptativo
+		placeDefensesFusion(freeCells, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, defenses);
+
+		++r2;
+    } while(c2.tiempo() < 1.0);
+    c2.parar();
+
+    cronometro c3;
+    long int r3 = 0;
+    c3.activar();
+    do { // Este do-while tiene el esequema de medida adaptativo
+		placeDefensesRapido(freeCells, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, defenses);
+
+		++r3;
+    } while(c3.tiempo() < 1.0);
+    c3.parar();
+
+    cronometro c4;
+    long int r4 = 0;
+    c4.activar();
+    do { // Este do-while tiene el esequema de medida adaptativo
+		placeDefensesMonticulo(freeCells, nCellsWidth, nCellsHeight, mapWidth, mapHeight, obstacles, defenses);
+
+		++r4;
+    } while(c4.tiempo() < 1.0);
+    c4.parar();
+
+
+    std::cout << (nCellsWidth * nCellsHeight) << '\t' << c1.tiempo() / r1 << '\t' << c2.tiempo() / r2 << '\t' << c3.tiempo() / r3 << '\t' << c4.tiempo() / r4 << std::endl;
 }
