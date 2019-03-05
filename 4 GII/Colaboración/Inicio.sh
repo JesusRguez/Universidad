8726fde0a1201bb576b14d6c4c52efaae6650704
JesusRguez <jesusrh1997@gmail.com>
2019-03-05 20:58:54 +0100

    Completaddo el script de inicio

--- 4 GII/Colaboración/Inicio.sh
+++ 4 GII/Colaboración/Inicio.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-tarjetas=(192.168.1.2 192.168.1.3)
-for t in "${tarjetas[@]}"
+direcciones=(192.168.1.39 192.168.1.40)
+tarjetas=(zybo1 zybo2)
+a=0
+for d in "${direcciones[@]}"
 do
-	echo Haciendo ping a $t
-	ping -c 3 $t
+	echo Comprobando la conexión con ${tarjetas[a]}.
+	string=$(ping -c 3 $d)
+	if [[ $string == *100%\ packet\ loss* ]]; then
+		echo Tarjeta ${tarjetas[a]} desconectada. Compruebe la conexion.
+	else
+		echo Tarjeta ${tarjetas[a]} conectada.
+	fi
+	let a=a+1
 done
