ff440b49d813628298f85d16e7fe17238245720a
JesusRguez <jesusrh1997@gmail.com>
2019-03-11 09:32:15 +0100

    Arreglado el script del laboratorio

--- 4 GII/Colaboración/Inicio.sh
+++ 4 GII/Colaboración/Inicio.sh
@@ -5,12 +5,17 @@ tarjetas=(zybo1 zybo2)
 a=0
 for d in "${direcciones[@]}"
 do
-	echo Comprobando la conexión con ${tarjetas[a]}.
-	string=$(ping -c 3 $d)
-	if [[ $string == *100%\ packet\ loss* ]]; then
-		echo Tarjeta ${tarjetas[a]} desconectada. Compruebe la conexion.
+	if [[ $1 = "-v" ]]; then
+		echo Comprobando la conexión con ${tarjetas[a]}.
+		ping -c 3 $d
 	else
-		echo Tarjeta ${tarjetas[a]} conectada.
+		echo Comprobando la conexión con ${tarjetas[a]}.
+		string=$(ping -c 3 $d)
+		if [[ $string == *100%\ packet\ loss* ]]; then
+			echo Tarjeta ${tarjetas[a]} desconectada. Compruebe la conexión.$'\n'
+		else
+			echo Tarjeta ${tarjetas[a]} conectada.$'\n'
+		fi
 	fi
 	let a=a+1
 done
