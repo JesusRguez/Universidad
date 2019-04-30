d8fad7e439546f0d109b103a92f3d58a02027c4e
JesusRguez <jesusrh1997@gmail.com>
2019-04-30 09:21:23 +0200

    Seminario de POO

--- 2 GII/2do Semestre/POO/Curso 2018-2019/Seminarios/Seminario3.2.6.cpp
+++ 2 GII/2do Semestre/POO/Curso 2018-2019/Seminarios/Seminario3.2.6.cpp
@@ -1,9 +1,19 @@
 //Grupos impartidos por el profesor p
 const Profesor_Grupo::Grupo& Profesor_Grupo::impartidos(Profesor& p) const{
-	return directa[&p];
+	std::map<Profesor*, Grupo>::iterator it = directa.find(p);
+	if(it != directa.end()){
+		return directa[&p];
+	}else{
+		return NULL;
+	}
 }
 
 //Profesor que imparte el grupo g
 Profesor& Profesor_Grupo::impartido(Grupo& g) const{
-	return inversa[&g];
+	std::map<Grupo*, Profesor>::iterator it = inversa.find(g);
+	if(it != inversa.end()){
+		return inversa[&g];
+	}else{
+		return NULL;
+	}
 }
