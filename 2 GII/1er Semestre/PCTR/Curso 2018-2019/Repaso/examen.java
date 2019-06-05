417e77fa6125a184de3222f90c5463a3c79c7058
JesusRguez <jesusrh1997@gmail.com>
2019-06-05 13:52:07 +0200

    Creada la carpeta Repaso en PCTR

---
+++ 2 GII/1er Semestre/PCTR/Curso 2018-2019/Repaso/examen.java
@@ -0,0 +1,27 @@
+import java.util.concurrent.locks.*;
+public class examen implements Runnable
+{
+public static Integer p = new Integer(0);
+public static ReentrantLock r = new ReentrantLock();
+public int vMax;
+public examen(int v) {vMax = v;}
+public void run()
+{
+Object q=p;
+for(int cont=0;cont<vMax; cont++)
+{ r.lock(); p++; r.unlock();
+/*synchronized(q){try{q.wait();} //quitando esto sale 1001 y si lo dejamos, 11 por el wait()
+catch(InterruptedException e){}}*/}
+}
+public static void main(String[] args)
+throws InterruptedException
+{
+Thread [] h = new Thread[10];
+for(int i=0;i<h.length;i++)h[i]=new Thread(new examen(100));
+for(int i=0;i<h.length;i++)h[i].start();
+Thread j = Thread.currentThread();
+j.sleep(1000);
+p++;
+System.out.print(p);
+}
+}
